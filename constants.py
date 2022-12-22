bce0ca6f6b092d93e2fdaf43519c68d2a9d931e5
Bengisu Şimşek <118427441+bengisusimsek@users.noreply.github.com>
2022-12-23 02:51:36 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,25 @@
+BASE_DOMAIN_URL = "https://www.saucedemo.com/" 
+
+USERNAME_ID = "user-name" 
+USERNAME = "standard_user"
+
+PASSWORD_ID = "password" 
+PASSWORD = "secret_sauce"
+
+LOGİN_BTN = "login-button"
+LOGIN = "https://www.saucedemo.com/inventory.html" 
+
+ERROR = '//*[@data-test="error"]' 
+ERROR_LOGİN = "Epic sadface: Username and password do not match any user in this service"
+WRONG_PASSWORD = "wrong_password"
+
+USER_MENU = "react-burger-menu-btn" 
+MENU_TEXT = "Open Menu" 
+
+INVENTORY_LIST = '//*[@class="inventory_item"]'
+
+ADD_TO_CARD = "add-to-cart-sauce-labs-backpack" 
+PRODUCT_REMOVE_NAME = "remove-sauce-labs-backpack" 
+PRODUCT_REMOVE_TEXT = "REMOVE"
+BASKET = "shopping_cart_badge" 
+BASKET_COUNT = "1" 
